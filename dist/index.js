593322749c190a1c8be346990357ff189f01eec2
Misha Kav <misha.kav@gmail.com>
2022-01-21 17:11:43 +0200

    add reportOnlyChangedFiles

--- dist/index.js
+++ dist/index.js
@@ -13551,6 +13551,12 @@ const {
 const { getMultipleReport } = __nccwpck_require__(4158);
 
 const MAX_COMMENT_LENGTH = 65536;
+const FILE_STATUSES = Object.freeze({
+  ADDED: 'added',
+  MODIFIED: 'modified',
+  REMOVED: 'removed',
+  RENAMED: 'renamed',
+});
 
 const main = async () => {
   const token = core.getInput('github-token', { required: true });
@@ -13562,6 +13568,10 @@ const main = async () => {
     required: false,
   });
   const hideComment = core.getBooleanInput('hide-comment', { required: false });
+  const reportOnlyChangedFiles = core.getBooleanInput(
+    'report-only-changed-files',
+    { required: false }
+  );
   const defaultBranch = core.getInput('default-branch', { required: false });
   const covFile = core.getInput('pytest-coverage-path', { required: false });
   const xmlFile = core.getInput('junitxml-path', { required: false });
@@ -13571,10 +13581,12 @@ const main = async () => {
   });
   const { context, repository } = github;
   const { repo, owner } = context.repo;
+  const { eventName, payload } = context;
   const WATERMARK = `<!-- Pytest Coverage Comment: ${context.job} -->\n`;
   let finalHtml = '';
 
   const options = {
+    token,
     repository: repository || `${owner}/${repo}`,
     prefix: `${process.env.GITHUB_WORKSPACE}/`,
     covFile,
@@ -13585,18 +13597,24 @@ const main = async () => {
     hideReport,
     createNewComment,
     hideComment,
+    reportOnlyChangedFiles,
     defaultBranch,
     xmlTitle,
     multipleFiles,
   };
 
-  if (context.eventName === 'pull_request') {
-    options.commit = context.payload.pull_request.head.sha;
-    options.head = context.payload.pull_request.head.ref;
-    options.base = context.payload.pull_request.base.ref;
-  } else if (context.eventName === 'push') {
-    options.commit = context.payload.after;
+  if (eventName === 'pull_request') {
+    options.commit = payload.pull_request.head.sha;
+    options.head = payload.pull_request.head.ref;
+    options.base = payload.pull_request.base.ref;
+  } else if (eventName === 'push') {
+    options.commit = payload.after;
     options.head = context.ref;
+  }
+
+  if (options.reportOnlyChangedFiles) {
+    const changedFiles = await getChangedFiles(options);
+    console.log('changedFiles', JSON.stringify(changedFiles));
   }
 
   if (multipleFiles && multipleFiles.length) {
@@ -13659,11 +13677,9 @@ const main = async () => {
   const body = WATERMARK + finalHtml;
   const octokit = github.getOctokit(token);
 
-  const issue_number = context.payload.pull_request
-    ? context.payload.pull_request.number
-    : 0;
+  const issue_number = payload.pull_request ? payload.pull_request.number : 0;
 
-  if (context.eventName === 'push') {
+  if (eventName === 'push') {
     console.log('Create commit comment');
     await octokit.repos.createCommitComment({
       repo,
@@ -13673,7 +13689,7 @@ const main = async () => {
     });
   }
 
-  if (context.eventName === 'pull_request') {
+  if (eventName === 'pull_request') {
     if (createNewComment) {
       console.log('Creating a new comment');
 
@@ -13714,6 +13730,133 @@ const main = async () => {
         });
       }
     }
+  }
+};
+
+const getChangedFiles = async (options) => {
+  try {
+    const { context } = github;
+    const { eventName, payload } = context;
+    const { repo, owner } = context.repo;
+    const octokit = github.getOctokit(options.token);
+
+    // Define the base and head commits to be extracted from the payload
+    let base, head;
+
+    switch (eventName) {
+      case 'pull_request':
+        base = payload.pull_request?.base?.sha;
+        head = payload.pull_request?.head?.sha;
+        break;
+      case 'push':
+        base = payload.before;
+        head = payload.after;
+        break;
+      default:
+        core.setFailed(
+          `This action only supports pull requests and pushes, ${eventName} events are not supported. ` +
+            "Please submit an issue on this action's GitHub repo if you believe this in correct."
+        );
+    }
+
+    // Log the base and head commits
+    core.info(`Base commit: ${base}`);
+    core.info(`Head commit: ${head}`);
+
+    // Use GitHub's compare two commits API.
+    // https://developer.github.com/v3/repos/commits/#compare-two-commits
+    const response = await octokit.repos.compareCommits({
+      base,
+      head,
+      owner,
+      repo,
+    });
+
+    // Ensure that the request was successful.
+    if (response.status !== 200) {
+      core.setFailed(
+        `The GitHub API for comparing the base and head commits for this ${eventName} event returned ${response.status}, expected 200. ` +
+          "Please submit an issue on this action's GitHub repo."
+      );
+    }
+
+    // Ensure that the head commit is ahead of the base commit.
+    if (response.data.status !== 'ahead') {
+      core.setFailed(
+        `The head commit for this ${eventName} event is not ahead of the base commit. ` +
+          "Please submit an issue on this action's GitHub repo."
+      );
+    }
+
+    // Get the changed files from the response payload.
+    const files = response.data.files;
+    const all = [],
+      added = [],
+      modified = [],
+      removed = [],
+      renamed = [],
+      addedModified = [];
+
+    for (const file of files) {
+      const { filename, status } = file;
+
+      all.push(filename);
+
+      switch (status) {
+        case FILE_STATUSES.ADDED:
+          added.push(filename);
+          addedModified.push(filename);
+          break;
+        case FILE_STATUSES.MODIFIED:
+          modified.push(filename);
+          addedModified.push(filename);
+          break;
+        case FILE_STATUSES.REMOVED:
+          removed.push(filename);
+          break;
+        case FILE_STATUSES.RENAMED:
+          renamed.push(filename);
+          break;
+        default:
+          core.setFailed(
+            `One of your files includes an unsupported file status '${status}', expected ${Object.values(
+              FILE_STATUSES
+            ).join(',')}.`
+          );
+      }
+    }
+
+    // Format the arrays of changed files.
+    let allFormatted,
+      addedFormatted,
+      modifiedFormatted,
+      removedFormatted,
+      renamedFormatted,
+      addedModifiedFormatted;
+
+    allFormatted = all.join(',');
+    addedFormatted = added.join(',');
+    modifiedFormatted = modified.join(',');
+    removedFormatted = removed.join(',');
+    renamedFormatted = renamed.join(',');
+    addedModifiedFormatted = addedModified.join(',');
+
+    // Log the output values.
+    core.info(`All: ${allFormatted}`);
+    core.info(`Added: ${addedFormatted}`);
+    core.info(`Modified: ${modifiedFormatted}`);
+    core.info(`Removed: ${removedFormatted}`);
+    core.info(`Renamed: ${renamedFormatted}`);
+    core.info(`Added or modified: ${addedModifiedFormatted}`);
+
+    return {
+      [FILE_STATUSES.ADDED]: addedFormatted,
+      [FILE_STATUSES.MODIFIED]: modifiedFormatted,
+      [FILE_STATUSES.REMOVED]: removedFormatted,
+      [FILE_STATUSES.RENAMED]: renamedFormatted,
+    };
+  } catch (error) {
+    core.setFailed(error.message);
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ const {
 const { getMultipleReport } = require('./multiFiles');
 
 const MAX_COMMENT_LENGTH = 65536;
+const FILE_STATUSES = Object.freeze({
+  ADDED: 'added',
+  MODIFIED: 'modified',
+  REMOVED: 'removed',
+  RENAMED: 'renamed',
+});
 
 const main = async () => {
   const token = core.getInput('github-token', { required: true });
@@ -20,6 +26,10 @@ const main = async () => {
     required: false,
   });
   const hideComment = core.getBooleanInput('hide-comment', { required: false });
+  const reportOnlyChangedFiles = core.getBooleanInput(
+    'report-only-changed-files',
+    { required: false }
+  );
   const defaultBranch = core.getInput('default-branch', { required: false });
   const covFile = core.getInput('pytest-coverage-path', { required: false });
   const xmlFile = core.getInput('junitxml-path', { required: false });
@@ -34,6 +44,7 @@ const main = async () => {
   let finalHtml = '';
 
   const options = {
+    token,
     repository: repository || `${owner}/${repo}`,
     prefix: `${process.env.GITHUB_WORKSPACE}/`,
     covFile,
@@ -44,6 +55,7 @@ const main = async () => {
     hideReport,
     createNewComment,
     hideComment,
+    reportOnlyChangedFiles,
     defaultBranch,
     xmlTitle,
     multipleFiles,
@@ -56,6 +68,11 @@ const main = async () => {
   } else if (eventName === 'push') {
     options.commit = payload.after;
     options.head = context.ref;
+  }
+
+  if (options.reportOnlyChangedFiles) {
+    const changedFiles = await getChangedFiles(options);
+    console.log('changedFiles', JSON.stringify(changedFiles));
   }
 
   if (multipleFiles && multipleFiles.length) {
@@ -171,6 +188,133 @@ const main = async () => {
         });
       }
     }
+  }
+};
+
+const getChangedFiles = async (options) => {
+  try {
+    const { context } = github;
+    const { eventName, payload } = context;
+    const { repo, owner } = context.repo;
+    const octokit = github.getOctokit(options.token);
+
+    // Define the base and head commits to be extracted from the payload
+    let base, head;
+
+    switch (eventName) {
+      case 'pull_request':
+        base = payload.pull_request?.base?.sha;
+        head = payload.pull_request?.head?.sha;
+        break;
+      case 'push':
+        base = payload.before;
+        head = payload.after;
+        break;
+      default:
+        core.setFailed(
+          `This action only supports pull requests and pushes, ${eventName} events are not supported. ` +
+            "Please submit an issue on this action's GitHub repo if you believe this in correct."
+        );
+    }
+
+    // Log the base and head commits
+    core.info(`Base commit: ${base}`);
+    core.info(`Head commit: ${head}`);
+
+    // Use GitHub's compare two commits API.
+    // https://developer.github.com/v3/repos/commits/#compare-two-commits
+    const response = await octokit.repos.compareCommits({
+      base,
+      head,
+      owner,
+      repo,
+    });
+
+    // Ensure that the request was successful.
+    if (response.status !== 200) {
+      core.setFailed(
+        `The GitHub API for comparing the base and head commits for this ${eventName} event returned ${response.status}, expected 200. ` +
+          "Please submit an issue on this action's GitHub repo."
+      );
+    }
+
+    // Ensure that the head commit is ahead of the base commit.
+    if (response.data.status !== 'ahead') {
+      core.setFailed(
+        `The head commit for this ${eventName} event is not ahead of the base commit. ` +
+          "Please submit an issue on this action's GitHub repo."
+      );
+    }
+
+    // Get the changed files from the response payload.
+    const files = response.data.files;
+    const all = [],
+      added = [],
+      modified = [],
+      removed = [],
+      renamed = [],
+      addedModified = [];
+
+    for (const file of files) {
+      const { filename, status } = file;
+
+      all.push(filename);
+
+      switch (status) {
+        case FILE_STATUSES.ADDED:
+          added.push(filename);
+          addedModified.push(filename);
+          break;
+        case FILE_STATUSES.MODIFIED:
+          modified.push(filename);
+          addedModified.push(filename);
+          break;
+        case FILE_STATUSES.REMOVED:
+          removed.push(filename);
+          break;
+        case FILE_STATUSES.RENAMED:
+          renamed.push(filename);
+          break;
+        default:
+          core.setFailed(
+            `One of your files includes an unsupported file status '${status}', expected ${Object.values(
+              FILE_STATUSES
+            ).join(',')}.`
+          );
+      }
+    }
+
+    // Format the arrays of changed files.
+    let allFormatted,
+      addedFormatted,
+      modifiedFormatted,
+      removedFormatted,
+      renamedFormatted,
+      addedModifiedFormatted;
+
+    allFormatted = all.join(',');
+    addedFormatted = added.join(',');
+    modifiedFormatted = modified.join(',');
+    removedFormatted = removed.join(',');
+    renamedFormatted = renamed.join(',');
+    addedModifiedFormatted = addedModified.join(',');
+
+    // Log the output values.
+    core.info(`All: ${allFormatted}`);
+    core.info(`Added: ${addedFormatted}`);
+    core.info(`Modified: ${modifiedFormatted}`);
+    core.info(`Removed: ${removedFormatted}`);
+    core.info(`Renamed: ${renamedFormatted}`);
+    core.info(`Added or modified: ${addedModifiedFormatted}`);
+
+    return {
+      [FILE_STATUSES.ADDED]: addedFormatted,
+      [FILE_STATUSES.MODIFIED]: modifiedFormatted,
+      [FILE_STATUSES.REMOVED]: removedFormatted,
+      [FILE_STATUSES.RENAMED]: renamedFormatted,
+    };
+  } catch (error) {
+    core.setFailed(error.message);
   }
 };
 
