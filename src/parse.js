6cb470984476288c410b22082ae20b9163e2cada
Misha Kav <misha.kav@gmail.com>
2022-09-06 21:26:23 +0300

    fix

--- src/parse.js
+++ src/parse.js
@@ -22,26 +22,29 @@ const isValidCoverageContent = (data) => {
 const getCoverageReport = (options) => {
   const { covFile, covXmlFile } = options;
 
-  try {
-    const covFilePath = getPathToFile(covFile);
-    const content = getContentFile(covFilePath, covXmlFile != null);
-    const coverage = getTotalCoverage(content);
-    const isValid = isValidCoverageContent(content);
+  if (covXmlFile != null) {
+    try {
+      const covFilePath = getPathToFile(covFile);
+      const content = getContentFile(covFilePath);
+      const coverage = getTotalCoverage(content);
+      const isValid = isValidCoverageContent(content);
 
-    if (content && !isValid) {
-      core.error(`Coverage file "${covFilePath}" has bad format or wrong data`);
+      if (content && !isValid) {
+        // prettier-ignore
+        core.error(`Coverage file "${covFilePath}" has bad format or wrong data`);
+      }
+
+      if (content && isValid) {
+        const html = toHtml(content, options);
+        const total = getTotal(content);
+        const warnings = getWarnings(content);
+        const color = getCoverageColor(total ? total.cover : '0');
+
+        return { html, coverage, color, warnings };
+      }
+    } catch (error) {
+      core.error(`Generating coverage report. ${error.message}`);
     }
-
-    if (content && isValid) {
-      const html = toHtml(content, options);
-      const total = getTotal(content);
-      const warnings = getWarnings(content);
-      const color = getCoverageColor(total ? total.cover : '0');
-
-      return { html, coverage, color, warnings };
-    }
-  } catch (error) {
-    core.error(`Generating coverage report. ${error.message}`);
   }
 
   return { html: '', coverage: '0', color: 'red', warnings: 0 };
